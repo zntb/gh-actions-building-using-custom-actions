9b748d00e9745accbadf072a2ba478874c76adf3
zntb <zenetibi@yahoo.com>
2024-12-03 12:34:11 +0200

    fix main.js

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -8,11 +8,12 @@ function run() {
   const bucketRegion = core.getInput('bucket-region', { required: true });
   const distFolder = core.getInput('dist-folder', { required: true });
 
-  // 2) Upload files to S3
-  const s3Uri = `s3://${bucket}/${distFolder}`;
+  // 2) Upload files
+  const s3Uri = `s3://${bucket}`;
   exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion}`);
 
-  core.notice('Hello from my custom javascript action!');
+  const websiteUrl = `http://${bucket}.s3-website-${bucketRegion}.amazonaws.com`;
+  core.setOutput('website-url', websiteUrl); // echo "website-url=..." >> $GITHUB_OUTPUT
 }
 
 run();
